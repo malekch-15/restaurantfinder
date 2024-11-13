2437e4723284a73b051ce7f82a2ebca8f23e9081
Aygul <gulya@gmx.net>
2024-11-13 16:29:58 +0100

    add integration test for getRestaurantById

--- backend/src/test/java/RestaurantIntegrationTest.java
+++ backend/src/test/java/RestaurantIntegrationTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,24 +21,57 @@ class RestaurantIntegrationTest {
     @Autowired
     RestaurantRepo restaurantRepo;
 
+    @BeforeEach
+    void setup() {
+        restaurantRepo.deleteAll();
+        createRestaurant("1", "Testrestaurant", "Testcity", "testkategorie",
+                "test description", WishlistStatus.ON_WISHLIST);
+    }
 
+    private RestaurantModel createRestaurant(String id, String name, String city, String category,
+                                             String description, WishlistStatus wishlistStatus) {
+        RestaurantModel restaurantModel = new RestaurantModel(id, name, city, category, description, wishlistStatus);
+        return restaurantRepo.save(restaurantModel); // Restaurant speichern
+    }
 
     @Test
     void getAllRestaurants_expectListWithOneRestaurant_whenOneRestaurantSaved() throws Exception {
-        //GIVEN
-        RestaurantModel restaurantModel=new RestaurantModel("1","Testrestaurant","Testcity",
-                "testkategorie","test description", WishlistStatus.ON_WISHLIST);
-        restaurantRepo.save(restaurantModel);
 
         //WHEN
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/restaurant")
-        )
+                        MockMvcRequestBuilders.get("/api/restaurant")
+                )
                 //THEN
                 .andExpect(
-                MockMvcResultMatchers.status().isOk())
+                        MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("""
-                                                                                        [
+                                                                                       [ 
+                                                                                         {
+                            "id": "1",
+                            "name": "Testrestaurant",
+                            "city": "Testcity",
+                            "category": "testkategorie",
+                            "description": "test description",
+                            "status": "ON_WISHLIST"
+                                                                                         }]
+                        
+                        """
+                ));
+
+    }
+
+    @Test
+    void getRestaurantById_returnRestaurantWithId1_whenRestaurantWithId1Saved() throws Exception {
+
+        //WHEN
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/restaurant/1")
+                )
+                //THEN
+                .andExpect(
+                        MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        
                                                                                          {
                             "id": "1",
                             "name": "Testrestaurant",
@@ -46,10 +80,12 @@ class RestaurantIntegrationTest {
                             "description": "test description",
                             "status": "ON_WISHLIST"
                                                                                          }
-                                                                                        ]
+                        
                         """
                 ));
 
     }
+
 }
+
 
